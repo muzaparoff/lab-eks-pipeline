e3dff63772cee9f78e0630712ff4617f3b9a8f04
muzaparoff <muzyukins@gmail.com>
2025-04-01 17:15:22 +0300

    fix: github actions with ci/cd

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "node_group" {
   ingress {
     description = "Allow inter-node communication"
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     self        = true
   }
